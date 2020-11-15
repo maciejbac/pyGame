620906f6ddb52ebe013770015029928e5f1bcbfc
Maciej <macko939@gmail.com>
2020-11-15 14:55:01 +0000

    window builder and skeleton of the program

--- pyGame.py
+++ pyGame.py
@@ -1,1 +1,17 @@
+import pygame
 
+running = True
+pygame.display.set_caption('Ball game')
+(width, heigth) = (300, 300)
+background_color = (120, 160, 250)
+
+screen = pygame.display.set_mode((width, heigth))
+screen.fill(background_color)
+
+pygame.draw.circle(screen, (0, 0, 255), (150, 50), 15, 1)
+pygame.display.flip()
+
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
