91aa7378076d664bc8ca6329df6093c676978920
Maciej <macko939@gmail.com>
2020-11-15 15:39:43 +0000

    added random colors

--- pyGame.py
+++ pyGame.py
@@ -11,14 +11,14 @@ background_color = (120, 160, 250)
 
 class Particle:
 	def __init__(self, x, y, size, colour):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.colour = colour
-        self.thickness = 50
+		self.x = x
+		self.y = y
+		self.size = size
+		self.colour = colour
+		self.thickness = 50
 
-    def display(self):
-        pygame.draw.circle(screen, self.colour, (self.x, self.y), self.size, self.thickness)
+	def display(self):
+		pygame.draw.circle(screen, self.colour, (self.x, self.y), self.size, self.thickness)
 
 
 screen = pygame.display.set_mode((width, height))
@@ -28,17 +28,17 @@ particle_count = 10
 my_particles = []
 
 for n in range(particle_count):
-    size = random.randint(10, 20)
-    x = random.randint(size, width - size)
-    y = random.randint(size, height - size)
-    my_particles.append(Particle(x, y, size, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))))
+	size = random.randint(10, 20)
+	x = random.randint(size, width - size)
+	y = random.randint(size, height - size)
+	my_particles.append(Particle(x, y, size, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))))
 
 for particle in my_particles:
-    particle.display()
+	particle.display()
 
 pygame.display.flip()
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
