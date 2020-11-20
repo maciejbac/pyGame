6a51fa7d091d230179368eef6fddc0906d8d39ca
macko939 <macko939@gmail.com>
2020-11-20 16:53:32 +0000

    Added particle_max_size

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ running = True
 (width, height) = (800, 600)
 background_color = (120, 160, 250)
 pygame.display.set_caption('Ball game')
-particle_count = 20
+particle_count = 40
 gravity = (math.pi, 0.03)
 drag = 0.999
 elasticity = 0.85
+particle_max_size = 20
 
 # Initialize the screen object
 screen = pygame.display.set_mode((width, height))
@@ -30,7 +31,6 @@ class Particle:
         self.x = x
         self.y = y
         self.size = size
-
         self.mass = mass
 
         # TEMPORARY: set random colour and thickness, TODO: add support for manual colour selection
@@ -121,7 +121,7 @@ def collide(p1, p2):
 
 # Create a number of Particle objects using random values to populate the screen
 for n in range(particle_count):
-    particle_size = random.randint(10, 40)
+    particle_size = random.randint(10, particle_max_size)
     particle_density = 1
 
     particle_x = random.randint(particle_size, width - particle_size)
