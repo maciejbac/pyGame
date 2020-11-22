ef002b69951634db125a0cadb4bc60ef25396636
macko939 <macko939@gmail.com>
2020-11-22 11:45:35 +0000

    Renamed width and height variables

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ import math
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 running = True
-(width, height) = (800, 600)
+(window_width, window_height) = (800, 600)
 background_color = (120, 160, 250)
 pygame.display.set_caption('Ball game')
 particle_count = 25
+gravity = 'off'
 gravity = (math.pi, 0.02)
 mass_of_air = 0.2
 elasticity = 0.95
 particle_max_size = 20
 
 # Initialize the screen object
-screen = pygame.display.set_mode((width, height + 50))
+screen = pygame.display.set_mode((window_width, window_height + 50))
 
 # Initialize empty particle array
 my_particles = []
@@ -52,16 +53,17 @@ class Particle:
         self.x += math.sin(self.angle) * self.speed
         self.y -= math.cos(self.angle) * self.speed
 
-        # Gravity function that adds a pre-defined gravity vector to the existing movement vector of the particle
-        (self.angle, self.speed) = add_vectors(self.angle, self.speed, *gravity)
+        if gravity == 'on':
+            # Gravity function that adds a pre-defined gravity vector to the existing movement vector of the particle
+            (self.angle, self.speed) = add_vectors(self.angle, self.speed, *gravity)
 
         # Multiply particle's speed by the drag to introduce air drag
         self.speed *= self.drag
 
     # if statements that detect and respond to collisions. Each if statement handles 1 out of 4 sides of the window.
     def bounce(self):
-        if self.x > width - self.size:
-            self.x = 2 * (width - self.size) - self.x
+        if self.x > window_width - self.size:
+            self.x = 2 * (window_width - self.size) - self.x
             self.angle = - self.angle
             self.speed *= elasticity
 
@@ -70,8 +72,8 @@ class Particle:
             self.angle = - self.angle
             self.speed *= elasticity
 
-        if self.y > height - self.size:
-            self.y = 2 * (height - self.size) - self.y
+        if self.y > window_height - self.size:
+            self.y = 2 * (window_height - self.size) - self.y
             self.angle = math.pi - self.angle
             self.speed *= elasticity
 
@@ -81,6 +83,7 @@ class Particle:
             self.speed *= elasticity
 
 
+# Function that adds two vectors together - used to handle gravity and collisions
 def add_vectors(angle1, length1, angle2, length2):
     x = math.sin(angle1) * length1 + math.sin(angle2) * length2
     y = math.cos(angle1) * length1 + math.cos(angle2) * length2
@@ -127,8 +130,8 @@ for n in range(particle_count):
     particle_size = random.randint(10, particle_max_size)
     particle_density = 1.00
 
-    particle_x = random.randint(particle_size, width - particle_size)
-    particle_y = random.randint(particle_size, height - particle_size)
+    particle_x = random.randint(particle_size, window_width - particle_size)
+    particle_y = random.randint(particle_size, window_height - particle_size)
 
     particle = Particle(particle_x, particle_y, particle_size, particle_density * particle_size ** 2)
     # particle.colour = (200 - particle_density * 10, 200 - particle_density * 10, 255)
@@ -173,5 +176,5 @@ while running:
             collide(particle, particle2)
 
     # Swap the frame buffer
-    pygame.draw.line(screen, (100, 100, 255), (0, height), (width, height))
+    pygame.draw.line(screen, (100, 100, 255), (0, window_height), (window_width, window_height))
     pygame.display.flip()
